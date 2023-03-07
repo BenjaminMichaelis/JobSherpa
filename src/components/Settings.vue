2801e2eeb5a5821024637a8a70a2c88ae8fe3619
njacksonwsu <nathan.jackson@wsu.edu>
2023-03-06 21:36:18 -0800

    Added settings page
    Settings page that allows changing theme and template for editing user profile.

---
+++ src/components/Settings.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-navigation-drawer expand-on-hover rail>
+      <v-list>
+        <v-list-item
+          prepend-avatar="src\assets\default_profile.png"
+          title="Default Profile"
+          subtitle="DefaultProfile@gmailcom"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item
+          @click="toggleTheme"
+          prepend-icon="mdi-theme-light-dark"
+          title="Change Theme"
+          value="mytheme"
+        >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main style="height: 250px"></v-main>
+  </v-layout>
+</template>
+<script>
+import { useTheme } from "vuetify";
+
+export default {
+  setup() {
+    const theme = useTheme();
+
+    return {
+      theme,
+      toggleTheme: () =>
+        (theme.global.name.value = theme.global.current.value.dark
+          ? "light"
+          : "dark"),
+    };
+  },
+};
+</script>
