e8dc19c2597a9ed1e3307d6565a11edca718d2bc
Benjamin Michaelis <benjamin@michaelis.net>
2023-03-07 10:07:20 -0800

    Update Router

---
+++ src/router.ts
@@ -0,0 +1,27 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("./views/Login.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("./views/Register.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
