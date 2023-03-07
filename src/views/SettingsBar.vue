3f254cd5a4126cbdeb777229eada779c2fe553c1
njacksonwsu <nathan.jackson@wsu.edu>
2023-03-06 21:36:18 -0800

    Added settings page
    Settings page that allows changing theme and template for editing user profile.

---
+++ src/views/SettingsBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <SettingsBar />
+  <EditProfile />
+</template>
+
+<script lang="ts" setup>
+import SettingsBar from "@/components/Settings.vue";
+import EditProfile from "@/components/EditProfile.vue";
+</script>
