5b1a2e7f45a502cd84ae42dc6c42ce08d950588f
Benjamin Michaelis <benjamin@michaelis.net>
2023-03-06 18:13:57 -0800

    iterate over job cards

---
+++ src/components/JobCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+    <v-card-item>
+      <v-card-title>Example Job Title</v-card-title>
+
+      <v-card-subtitle>
+        <span class="me-1">Sample Company</span>
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+      <v-row align="center" class="mx-0"> </v-row>
+
+      <div class="my-4 text-subtitle-1">Salary: $40,000</div>
+
+      <div class="px-4">
+        <span class="text-subtitle-1">Skills</span>
+        <v-chip-group v-model="selection">
+          <v-chip>SQL</v-chip>
+
+          <v-chip>React</v-chip>
+
+          <v-chip>C#</v-chip>
+
+          <v-chip>C++</v-chip>
+        </v-chip-group>
+      </div>
+    </v-card-text>
+
+    <v-divider class="mx-4 mb-1"></v-divider>
+
+    <v-card-title>Upcoming</v-card-title>
+
+    <v-card-text>
+      <v-checkbox label="Submit Cover Letter on February 3"></v-checkbox>
+      <v-checkbox label="Interview #1 on March 5"></v-checkbox>
+    </v-card-text>
+    <v-divider class="mx-4 mb-1"></v-divider>
+
+    <v-card-title>Recent Activity</v-card-title>
+    <v-card-text>
+      Submitted Resume on January 8th
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+const loading = false;
+const selection = 1;
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,40 @@
 <template>
-  <HelloWorld />
+  <v-container class="pa-10">
+    <v-row>
+      <h1>Welcome User</h1>
+    </v-row>
+    <v-row>
+      <v-col v-for="job in jobs" :key="job.id" cols="2" lg="3">
+        <JobCard />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-import HelloWorld from "@/components/HelloWorld.vue";
+import JobCard from "@/components/JobCard.vue";
+
+
+let jobs = [
+  {
+    id: 1,
+    title: "Software Engineer",
+    value: "Google"
+  },
+  {
+    id: 2,
+    title: "Waitress",
+    value: "The Restaurant"
+  },
+  {
+    id: 3,
+    title: "Basketball Player",
+    value: "Chicago Bulls"
+  },
+  {
+    id: 4,
+    title: "Student",
+    value: "WSU"
+  }
+];
 </script>
