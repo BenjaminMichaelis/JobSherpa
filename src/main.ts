d88c63d10f5bcd41db43475624f6e4ea0e971407
Benjamin Michaelis <benjamin@michaelis.net>
2023-03-09 12:16:06 -0800

    Add 404, restructure router

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ import { createApp } from "vue";
 // Plugins
 import { registerPlugins } from "@/plugins";
 
-const app = createApp(App);
+// Router
+import router from "./router";
+
+const app = createApp(App).use(router);
 
 registerPlugins(app);
 
